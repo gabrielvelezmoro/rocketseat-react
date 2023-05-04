afb6a4045eebb1af0aa99a72a6c2bdf9346a2cf7
gabri <gabrieuvelezmoro@gmail.com>
2023-05-04 13:44:43 -0300

    style: add styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import styled from 'styled-components'
+
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257e6;
+`
 
 export function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Title className='title'>hello world</Title>
     </div>
   );
 }
