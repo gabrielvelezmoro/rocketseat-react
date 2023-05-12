ca398decf7fda80102c5ac33a6720a5f38f45d26
gabri <gabrieuvelezmoro@gmail.com>
2023-05-12 11:15:55 -0300

    feat: lista nova transação

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -20,7 +20,7 @@ interface ITransctionsProviderProps {
 }
 interface ITransactionsContextData {
     transactions: ITransaction[];
-    createTransaction: (transaction: TransactionInput) => void;
+    createTransaction: (transaction: TransactionInput) => Promise<ITransaction>;
 }
 
 export const TransactionsContext = createContext<ITransactionsContextData>(
@@ -36,8 +36,16 @@ export function TransactionsProvider({children}: ITransctionsProviderProps) {
             .then(response => setTransactions(response.data.transactions))
     },[])
 
-     function createTransaction(transaction: TransactionInput){
-        api.post('/transactions', transaction)
+    async function createTransaction(transactionInput: TransactionInput): Promise<ITransaction>{
+        const response = await api.post('/transactions', {...transactionInput, createdAt: new Date()})
+        const {transaction} = response.data
+
+        setTransactions([
+            ...transactions,
+            transaction
+        ])
+
+        return transaction
     }
 
 
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -20,19 +20,26 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
     const [title, setTitle] = useState('') ;
     const [amount, setAmount] = useState(0) ;
     const [category, setCategory] = useState('') ;
-    const [type, setType] = useState('withdraw') ;
+    const [type, setType] = useState('deposit') ;
 
     const {createTransaction} = useContext(TransactionsContext)
 
-    function handleCreateNewTransaction(event: FormEvent){
+    async function handleCreateNewTransaction(event: FormEvent){
         event.preventDefault();
         
-        createTransaction({
+        const response = await createTransaction({
             title, 
             amount, 
             category, 
             type
         })
+        console.log(response)
+        setTitle('');
+        setAmount(0);
+        setCategory('');
+        setType('deposit')
+
+        onRequestClose()
     }
 
     return (
